b2366462ab70b25a3ff1098971d631945abe1409
GiovasGit <125149518+GiovasGit@users.noreply.github.com>
2023-08-07 17:40:34 +0200

    redis.py

--- redis.py
+++ redis.py
@@ -3,10 +3,7 @@ The aim is to store in Redis the final
 dataframe containing the computed risks for
 each city. The df will be stored in form of
 document. It is possible both store and
-query the document through Redis OM.
-Better thing would be to be able to fetch
-from redis single rows for single city risk.
-Maybe different implementation to do that
+query the document for all cities or one city
 '''
 from meteo import meteo_connector
 from meteo_spark import Spark_session
