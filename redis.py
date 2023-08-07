0ce564272939b72c43cf2a3cc7100405d62d03d1
GiovasGit <125149518+GiovasGit@users.noreply.github.com>
2023-08-07 17:38:48 +0200

    redis.py

--- redis.py
+++ redis.py
@@ -4,16 +4,19 @@ dataframe containing the computed risks for
 each city. The df will be stored in form of
 document. It is possible both store and
 query the document through Redis OM.
-Cooler thing would be to be able to fetch
+Better thing would be to be able to fetch
 from redis single rows for single city risk.
 Maybe different implementation to do that
 '''
 from meteo import meteo_connector
 from meteo_spark import Spark_session
+import json
+import redis
+from redis.commands.json.path import Path
 
 # stuff to be imported from other scripts
 # dictionary with data
-filepath = '/home/giovanni/Scrivania/documentazione3b/loc_gb.csv' #to put in  'requirements'(?)
+filepath = '/home/giovanni/Scrivania/documentazione3b/loc_gb.csv'
 my_connector = meteo_connector(filepath)
 diz = my_connector.info_dict()
 
@@ -23,9 +26,12 @@ session.start_session()
 rows = session.from_dict_to_rows(diz)
 modified_rows = session.modify_rows(rows) #tuples!
 df = session.final_df(modified_rows) #dataframe to be converted to json
-#df.show()
 
 def df_json(my_df):
+    '''
+    converts the Spark dataframe
+    given as input into a JSON file
+    '''
     res = {}
     json_str =  my_df.toJSON().collect() #list of JSON strings
     for my_json in json_str:
@@ -33,15 +39,14 @@ def df_json(my_df):
         res[json_dict['city']] = json_dict
     return res
 
-#print(df_json(df))
-
+# Store the resulting JSON into Redis and retrieve risk computations first for all cities, then for the desired city
 data = df_json(df)
 
-#print(data)
-
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis()
 r.json().set('doc', '$', data) #putting data into redis
-doc = r.json().get('doc', '$') #instantiating a variable with all data
 
-print(doc) #retrieving all data
-#aberdeen = r.json().get('doc', '$.Aberdeen')
+doc = r.json().get('doc', '$') #instantiating a variable with risk data for all cities
+print(doc) #retrieving risk data for all cities
+
+aberdeen = r.json().get('doc', '$.Aberdeen') #instantiating a variable with risk data for one city
+print(aberdeen) #retrieving risk data for one city
